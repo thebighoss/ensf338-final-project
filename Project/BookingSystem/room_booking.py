6f4608330fee3e286f10ccce071199751537c8e7
thebighoss <afnan.hossain@ucalgary.ca>
2026-04-09 07:38:29 +0000

    room booking type hint fully done? needs double checking to ensure compatibility

--- Project/BookingSystem/room_booking.py
+++ Project/BookingSystem/room_booking.py
@@ -6,9 +6,11 @@ import numpy as np
 import tkinter as tk
 from tkinter import ttk
 import random as rd
+from typing import Any
 from DataStructures.LifoRingBuffer import LifoRingBuffer as lifo
 import RequestPipeline.request_pipeline as rp
 import DataStructures.AVL as avl
+from GlobalObjects.objects import Building, Floor, Room
 
 
 def helper_build_times(total_increments: int) -> list[str]:
@@ -25,7 +27,7 @@ def helper_build_times(total_increments: int) -> list[str]:
 
 
 class CampusWrapper():
-    def __init__(self, building, floor, room, data = FileNotFoundError) -> None:
+    def __init__(self, building: Building, floor: Floor, room: Room, data: Any = FileNotFoundError) -> None:
         self.building = building
         self.floor = floor
         self.room = room
@@ -34,7 +36,7 @@ class CampusWrapper():
 
 def time_str_to_float(t: str) -> float:
     h, m = map(int, t.split(":"))
-    return h + m / 60  # 12:30 → 12.5, 13:00 → 13.0
+    return h + (m / 60)  # 12:30 → 12.5, 13:00 → 13.0
 
 booking_search = avl.AVLTree()
 
@@ -46,28 +48,33 @@ class BookingSystem:
     def get_daily_booking(self, index: int) -> "DailyBooking | None": # the quotes are there because dailybooking is defined later in the program, but setting it as return type now shows a warning when type hinting.
         return self.daily_bookings.access_at_index(index)
     
-    def book_room(self, index: int, start_time: int, booker_name: str, booking_type: str) -> None:
+    def book_room(self, index: int, start_time: float, booker_name: str, booking_type: str) -> None:
         booking = self.daily_bookings.access_at_index(index)
+        timeIndex = int(start_time*2) #if a float value like 3.5 is given, this just to make sure it's indexable
         if booking is not None: #access_at_index method has return type -> T | None. This is to ensure it accounts for that.
-            booking.hourly_bookings[start_time].booker_name = booker_name
-            booking.hourly_bookings[start_time].booking_type = booking_type
+            booking.hourly_bookings[timeIndex].booker_name = booker_name
+            booking.hourly_bookings[timeIndex].booking_type = booking_type
             booking_search.insert(avl.Node(avl.hash_str_to_int(booker_name),self))
     
-    def delete_booking(self, index: int, start_time: int) -> None:
+    def delete_booking(self, index: int, start_time: float) -> None:
         bookingToDelete = self.daily_bookings.access_at_index(index)
+        timeIndex = int(start_time*2)
         if bookingToDelete is not None: #same as above
-            booking_search.delete(avl.hash_str_to_int(bookingToDelete.hourly_bookings[start_time].booker_name))
-            bookingToDelete.hourly_bookings[start_time].booker_name = "None"
-            bookingToDelete.hourly_bookings[start_time].booking_type = "Vacant"
+            bookingToDelete = bookingToDelete.hourly_bookings[timeIndex] # update to make it look cleaner
+            if bookingToDelete.booker_name != "None": #check if its already vacant
+                booking_search.delete(avl.hash_str_to_int(bookingToDelete.booker_name))
+                bookingToDelete.booker_name = "None"
+                bookingToDelete.booking_type = "Vacant"
+            
 
 class HourlyBooking:
-    def __init__(self, start_time: int, end_time: int, booker_name: str = "None", booking_type: str = "Vacant") -> None:
+    def __init__(self, start_time: float, end_time: float, booker_name: str = "None", booking_type: str = "Vacant") -> None:
         self.start_time = start_time
         self.end_time = end_time
         self.booker_name = booker_name
         self.booking_type = booking_type
     
-    def update_booking(self, booker_name: str, booking_type: str, campus_info) -> None:
+    def update_booking(self, booker_name: str, booking_type: str, campus_info: CampusWrapper) -> None:
         if booker_name == "None":
             booking_search.delete(avl.hash_str_to_int(self.booker_name))
         else:
@@ -83,4 +90,4 @@ class DailyBooking:
         for i in range(len(DailyBooking.times)-1): # 24/48 gives half hour incrmeents
             startTime = time_str_to_float(DailyBooking.times[i])
             endTime = time_str_to_float(DailyBooking.times[i+1])
-            self.hourly_bookings.append(HourlyBooking(int(startTime), int(endTime)))
+            self.hourly_bookings.append(HourlyBooking(startTime, endTime))
